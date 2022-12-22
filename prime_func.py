6767b1f07f4b17b25ddefd1d03abd72ade204118
sylvester_divine <sylvesterdivine@outlook.com>
2022-12-22 18:54:26 +0000

    file refinement

--- prime_func.py
+++ prime_func.py
@@ -3,7 +3,7 @@
 This file contains RSA factorization
 """
 def prime_factor(value):
-    for num in range(2, value):
+    for num in range(2, value+1):
         res = value % num
         if res == 0:
             return(value/num, num)
